b8446de916266427d39e8b90396e453772e3eeba
waitingfly <839992202@qq.com>
2016-07-01 13:42:42 +0800

    change goodsController and goodsMapper.xml

--- src/main/java/com/gentlemen/easybuy/controller/GoodsController.java
+++ src/main/java/com/gentlemen/easybuy/controller/GoodsController.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gentlemen.easybuy.entity.Goods;
 import com.gentlemen.easybuy.service.GoodsService;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -22,12 +25,15 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET, consumes = "application/json;charset=utf-8")
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ResponseBody
-    public String getSnap() throws JsonProcessingException {
+    public String getSnap(HttpServletResponse response) throws JsonProcessingException {
         System.out.println("`````````````````");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         List<Goods> list = goodsService.getGoodsSnap();
         System.out.println(new ObjectMapper().writeValueAsString(list));
         return new ObjectMapper().writeValueAsString(list);
     }
+
 }
